cd92962c6c231cdabda1c50be1a07571779e2eb9
jaydoncastro <89215931+jaydoncastro@users.noreply.github.com>
2025-10-05 20:03:34 -0700

    Create Person.java

---
+++ CISC191/Week2/Assignment5-6.1.md/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+   private int ageYears;
+   private String lastName;
+
+   public void setName(String userName) {
+      lastName = userName;
+   }
+
+   public void setAge(int numYears) {
+      ageYears = numYears;
+   }
+
+   public void printAll() {
+      System.out.print("Name: " + lastName);
+      System.out.print(", Age: " + ageYears);
+   }
+}
+ 
